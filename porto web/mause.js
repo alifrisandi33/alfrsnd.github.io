d29260287f707000d91c02fcd8d79f301e7110c1
alifrisandi33 <alifrisandi33@gmail.com>
2025-11-27 10:36:48 +0800

    Delete porto web directory

--- porto web/mause.js
+++
@@ -1,5 +0,0 @@
-const light = document.querySelector(".hero-light");
-
-document.addEventListener("mousemove", (e) => {
-    light.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
